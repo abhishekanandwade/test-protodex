22bda5443814ca908dc651239b3ee523949c276b
Abhishek <abhi.anandwade@gmail.com>
2025-10-09 14:09:50 +0530

    Auto-commit: Sync schema files

---
+++ schemas/test-package/0.0.29/handler/response/item_dex.go
@@ -0,0 +1,12 @@
+package response
+
+type ItemDex struct {
+	ItemID      int64
+	Description string
+	HsnCode     string
+	Type        string
+	City        string
+	State       string
+	Country     string
+	Notes       string
+}
