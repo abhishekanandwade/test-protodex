9552f5b0eb83aee53ee15a692a6101d27ae2497c
Abhishek <abhi.anandwade@gmail.com>
2025-10-09 16:08:58 +0530

    Auto-commit: Sync schema files

---
+++ schemas/new-package/0.0.2/handler/response/pli_dex.go
@@ -0,0 +1,12 @@
+package response
+
+type Pli struct {
+	Pli_status  int32
+	Pli_notes   string
+	Pli_name    string
+	Pli_Type    string
+	Pli_City    string
+	Pli_State   string
+	Pli_Country string
+	Pli_Notes   string
+}
